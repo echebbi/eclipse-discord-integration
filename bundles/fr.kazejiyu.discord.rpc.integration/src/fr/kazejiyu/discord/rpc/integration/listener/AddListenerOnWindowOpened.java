8e707bdade99af46cb41dc09a6acf9913ee81d0d
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-04-19 23:51:59 +0200

    Notify Discord each time Eclipse's current selection changes

---
+++ bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/listener/AddListenerOnWindowOpened.java
@@ -0,0 +1,59 @@
+package fr.kazejiyu.discord.rpc.integration.listener;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWindowListener;
+import org.eclipse.ui.IWorkbenchWindow;
+
+/**
+ * Adds a given {@link ISelectionListener} to each window that opens.
+ * 
+ * @author Emmanuel CHEBBI
+ */
+public class AddListenerOnWindowOpened implements IWindowListener {
+	
+	/** The listener to add to each window that opens */
+	private final ISelectionListener listener;
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param listener
+	 * 			The listener that should be added to each window that opens.
+	 * 			Must not be {@code null}.
+	 */
+	public AddListenerOnWindowOpened(ISelectionListener listener) {
+		this.listener = requireNonNull(listener, "The listener must not be null");
+	}
+
+	@Override
+	public void windowOpened(IWorkbenchWindow window) {
+		addSelectionListener(window);
+	}
+
+	@Override
+	public void windowClosed(IWorkbenchWindow window) {
+		removeSelectionListener(window);
+	}
+
+	@Override
+	public void windowActivated(IWorkbenchWindow window) {}
+
+	@Override
+	public void windowDeactivated(IWorkbenchWindow window) {}
+
+	/** Adds {@code listener} as an ISelectionListener of {@code window} */
+    private void addSelectionListener(IWorkbenchWindow window) {
+        if (window != null) {
+            window.getSelectionService().addSelectionListener(listener);
+        }
+    }
+
+    /** Removes {@code listener} from {@code window}'s ISelectionListeners */
+    private void removeSelectionListener(IWorkbenchWindow window) {
+        if (window != null) {
+            window.getSelectionService().removeSelectionListener(listener);
+        }
+    }
+}
---
+++ bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/listener/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Classes used to listen for Eclipse's current selection.<br>
+ * <br>
+ * Each time the selection changes, Discord is notified and updates its overlay accordingly.
+ * 
+ * @author Emmanuel CHEBBI
+ */
+package fr.kazejiyu.discord.rpc.integration.listener;
