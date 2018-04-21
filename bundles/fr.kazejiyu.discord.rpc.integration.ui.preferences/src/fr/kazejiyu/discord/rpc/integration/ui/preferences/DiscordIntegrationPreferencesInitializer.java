a721d532fc113428a71fe30e9bd2a6a64ebd1838
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-04-21 17:40:13 +0200

    [TG-18 #done TG-19 #done] Populate Preferences page and persist the fields
    Add the fields "Show file's name" and "Show project's name" to Discord Integration's Preferences page.

---
+++ bundles/fr.kazejiyu.discord.rpc.integration.ui.preferences/src/fr/kazejiyu/discord/rpc/integration/ui/preferences/DiscordIntegrationPreferencesInitializer.java
@@ -0,0 +1,16 @@
+package fr.kazejiyu.discord.rpc.integration.ui.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+public class DiscordIntegrationPreferencesInitializer extends AbstractPreferenceInitializer {
+	
+	@Override
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		
+        store.setDefault("SHOW_FILE_NAME", true);
+        store.setDefault("SHOW_PROJECT_NAME", true);
+	}
+
+}
