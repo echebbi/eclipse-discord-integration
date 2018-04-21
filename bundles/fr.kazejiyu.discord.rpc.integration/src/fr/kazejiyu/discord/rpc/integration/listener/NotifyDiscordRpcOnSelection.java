dc02d148003703a38c649dca7d91348df5ccead5
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-04-21 14:18:14 +0200

    [TG-15 #done] Handle files that are not located within Eclipse's workspace

--- bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/listener/NotifyDiscordRpcOnSelection.java
+++ bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/listener/NotifyDiscordRpcOnSelection.java
@@ -1,10 +1,14 @@
 package fr.kazejiyu.discord.rpc.integration.listener;
 
+import java.io.File;
+import java.net.URI;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IURIEditorInput;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.EditorPart;
 
@@ -30,12 +34,24 @@ public class NotifyDiscordRpcOnSelection implements ISelectionListener {
 		
 		EditorPart editor = (EditorPart) part;
 		
+		// Generic file editor
 		if (editor.getEditorInput() instanceof IFileEditorInput) {
 			IFile inEdition = ((IFileEditorInput) editor.getEditorInput()).getFile();
 			IProject project = inEdition.getProject();
 			
 			String details = "Editing " + inEdition.getName();
-			String state = (project == null) ? "" : "Working on " + project.getName();
+			String state = (project != null) ? "Working on " + project.getName() : "";
+				
+			rpc.setInformations(details, state);
+		}
+		// Also handles Files located outside of the workspace
+		else if (editor.getEditorInput() instanceof IURIEditorInput) {
+			URI inEdition = ((IURIEditorInput) editor.getEditorInput()).getURI();
+			
+			File editedFile = new File(inEdition.getPath());
+			
+			String details = editedFile.exists() ? editedFile.getName() : "";
+			String state = "Unknown project";
 				
 			rpc.setInformations(details, state);
 		}
