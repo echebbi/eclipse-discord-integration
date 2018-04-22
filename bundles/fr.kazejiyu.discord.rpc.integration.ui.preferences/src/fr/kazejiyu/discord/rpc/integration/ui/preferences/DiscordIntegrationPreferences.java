59aa3001a44ee5b6a260e973d21e6c655baa01d5
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-04-22 03:12:52 +0200

    [TG-26 #done] Add fields in Preferences page specifying when to reset elapsed time

--- bundles/fr.kazejiyu.discord.rpc.integration.ui.preferences/src/fr/kazejiyu/discord/rpc/integration/ui/preferences/DiscordIntegrationPreferences.java
+++ bundles/fr.kazejiyu.discord.rpc.integration.ui.preferences/src/fr/kazejiyu/discord/rpc/integration/ui/preferences/DiscordIntegrationPreferences.java
@@ -3,6 +3,7 @@ package fr.kazejiyu.discord.rpc.integration.ui.preferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
@@ -23,6 +24,13 @@ public class DiscordIntegrationPreferences extends FieldEditorPreferencePage imp
     public void createFieldEditors() {
         addField(new BooleanFieldEditor("SHOW_FILE_NAME", "Show &file's name", getFieldEditorParent()));
         addField(new BooleanFieldEditor("SHOW_PROJECT_NAME", "Show &project's name", getFieldEditorParent()));
+        
+        addField(new RadioGroupFieldEditor("RESET_ELAPSED_TIME", "&Reset elapsed time:", 3,
+                new String[][] { { "On startup", "RESET_ELAPSED_TIME_ON_STARTUP" }, 
+        						 { "On new project", "RESET_ELAPSED_TIME_ON_NEW_PROJECT" },
+                				 { "On new file", "RESET_ELAPSED_TIME_ON_NEW_FILE" } },
+				getFieldEditorParent()
+		));
     }
     
 }
