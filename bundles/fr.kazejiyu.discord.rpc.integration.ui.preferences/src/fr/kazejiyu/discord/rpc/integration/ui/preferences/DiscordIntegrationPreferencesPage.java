bb886a44f52e2bd7b50adb57aaf5424fcfaa807f
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-04-24 23:13:33 +0200

    [TG-36 #done] Organize settings into visual groups

--- bundles/fr.kazejiyu.discord.rpc.integration.ui.preferences/src/fr/kazejiyu/discord/rpc/integration/ui/preferences/DiscordIntegrationPreferencesPage.java
+++ bundles/fr.kazejiyu.discord.rpc.integration.ui.preferences/src/fr/kazejiyu/discord/rpc/integration/ui/preferences/DiscordIntegrationPreferencesPage.java
@@ -11,7 +11,7 @@ import org.eclipse.ui.preferences.ScopedPreferenceStore;
 public class DiscordIntegrationPreferencesPage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
 	public DiscordIntegrationPreferencesPage() {
-		super(GRID);
+		super(FLAT);
 	}
 
 	@Override
@@ -22,8 +22,15 @@ public class DiscordIntegrationPreferencesPage extends FieldEditorPreferencePage
 
 	@Override
     public void createFieldEditors() {
-        addField(new BooleanFieldEditor("SHOW_FILE_NAME", "Show &file's name", getFieldEditorParent()));
-        addField(new BooleanFieldEditor("SHOW_PROJECT_NAME", "Show &project's name", getFieldEditorParent()));
+		GroupFieldEditor group = new GroupFieldEditor("Privacy", getFieldEditorParent());
+		
+        BooleanFieldEditor showFileName = new BooleanFieldEditor("SHOW_FILE_NAME", "Show &file's name", group.getFieldEditorParent());
+        BooleanFieldEditor showProjectName = new BooleanFieldEditor("SHOW_PROJECT_NAME", "Show &project's name", group.getFieldEditorParent());
+        
+        group.addFieldEditor(showFileName);
+        group.addFieldEditor(showProjectName);
+        
+        addField(group);
         
         addField(new RadioGroupFieldEditor("RESET_ELAPSED_TIME", "&Reset elapsed time:", 3,
                 new String[][] { { "On startup", "RESET_ELAPSED_TIME_ON_STARTUP" }, 
@@ -31,6 +38,7 @@ public class DiscordIntegrationPreferencesPage extends FieldEditorPreferencePage
                 				 { "On new file", "RESET_ELAPSED_TIME_ON_NEW_FILE" } },
 				getFieldEditorParent()
 		));
+        
     }
     
 }
