67d68df837a9ad7c1c254a08e7825c0f5a6b9654
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-04-28 13:10:44 +0200

    Implement equals() and hashCode() methods in RichPresence class

--- bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/core/RichPresence.java
+++ bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/core/RichPresence.java
@@ -158,4 +158,38 @@ public class RichPresence implements Cloneable {
 		return this;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((details == null) ? 0 : details.hashCode());
+		result = prime * result + (int) (startTimestamp ^ (startTimestamp >>> 32));
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof RichPresence))
+			return false;
+		RichPresence other = (RichPresence) obj;
+		if (details == null) {
+			if (other.details != null)
+				return false;
+		} else if (!details.equals(other.details))
+			return false;
+		if (startTimestamp != other.startTimestamp)
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		return true;
+	}
+	
 }
