03a5cc21595f1edc1748523f98caa2333ebbdbbe
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-04-30 16:06:57 +0200

    [TG-57 #done] Add a page to project Properties dialog

---
+++ bundles/fr.kazejiyu.discord.rpc.integration.ui.preferences/src/fr/kazejiyu/discord/rpc/integration/ui/preferences/properties/ProjectPropertiesPage.java
@@ -0,0 +1,20 @@
+package fr.kazejiyu.discord.rpc.integration.ui.preferences.properties;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class ProjectPropertiesPage extends PropertyPage implements IWorkbenchPropertyPage {
+
+	public ProjectPropertiesPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
