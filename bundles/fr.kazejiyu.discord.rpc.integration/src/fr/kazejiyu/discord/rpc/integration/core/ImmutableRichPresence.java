2c9c3812923a9437b9bfafb4e9668c79e4408572
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2019-02-26 15:27:01 +0100

    Update licenses

--- bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/core/ImmutableRichPresence.java
+++ bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/core/ImmutableRichPresence.java
@@ -1,12 +1,12 @@
-/*********************************************************************
-* Copyright (c) 2018 Emmanuel CHEBBI
-*
-* This program and the accompanying materials are made
-* available under the terms of the Eclipse Public License 2.0
-* which is available at https://www.eclipse.org/legal/epl-2.0/
-*
-* SPDX-License-Identifier: EPL-2.0
-**********************************************************************/
+/*******************************************************************************
+ * Copyright (C) 2018-2019 Emmanuel CHEBBI
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 package fr.kazejiyu.discord.rpc.integration.core;
 
 import static fr.kazejiyu.discord.rpc.integration.languages.Language.UNKNOWN;
