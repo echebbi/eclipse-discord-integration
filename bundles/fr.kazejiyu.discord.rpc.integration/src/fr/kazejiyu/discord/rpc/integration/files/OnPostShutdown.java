46869cd73ef2f34b89187ba6d9fee80ebada7c20
Emmanuel Chebbi <emmanuel.chebbi@outlook.fr>
2020-05-01 16:34:35 +0200

    Update copyright headers

--- bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/files/OnPostShutdown.java
+++ bundles/fr.kazejiyu.discord.rpc.integration/src/fr/kazejiyu/discord/rpc/integration/files/OnPostShutdown.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2018-2019 Emmanuel CHEBBI
+ * Copyright (C) 2018-2020 Emmanuel CHEBBI
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
